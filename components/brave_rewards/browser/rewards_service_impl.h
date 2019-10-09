814ad21f1d0baa193591126594dc4614d2a6a351
NejcZdovc <nejc.zdovc@3zsistemi.si>
2019-10-09 10:35:39 +0200

    Adds promotion and tokens database

--- components/brave_rewards/browser/rewards_service_impl.h
+++ components/brave_rewards/browser/rewards_service_impl.h
@@ -683,6 +683,21 @@ class RewardsServiceImpl : public RewardsService,
   void GetFirstContributionQueue(
     ledger::GetFirstContributionQueueCallback callback) override;
 
+  void InsertOrUpdatePromotion(
+      ledger::PromotionPtr info,
+      ledger::ResultCallback callback) override;
+
+  void GetPromotion(
+      const std::string& id,
+      ledger::GetPromotionCallback callback) override;
+
+  void InsertOrUpdateUnblindedToken(
+      ledger::UnblindedTokenPtr info,
+      ledger::ResultCallback callback) override;
+
+  void GetAllUnblindedTokens(
+      ledger::GetAllUnblindedTokensCallback callback) override;
+
   // end ledger::LedgerClient
 
   // Mojo Proxy methods
@@ -733,6 +748,14 @@ class RewardsServiceImpl : public RewardsService,
   void OnGetFirstContributionQueue(
     ledger::GetFirstContributionQueueCallback callback,
     ledger::ContributionQueuePtr info);
+
+  void OnGetPromotion(
+      ledger::GetPromotionCallback callback,
+      ledger::PromotionPtr info);
+
+  void OnGetAllUnblindedTokens(
+      ledger::GetAllUnblindedTokensCallback callback,
+      ledger::UnblindedTokenList list);
 
 #if defined(OS_ANDROID)
   ledger::Environment GetServerEnvironmentForAndroid();
