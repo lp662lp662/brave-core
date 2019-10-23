ffa96745a53960e0d586b60dd7f6b783f884cec0
Francois Marier <francois@brave.com>
2019-10-23 14:26:44 -0700

    Improve licensing notice (fixes brave/brave-browser#6605)
    This add a link to the license, build instructions as well as the
    corresponding source code.

--- chromium_src/chrome/browser/ui/webui/settings/about_handler.cc
+++ chromium_src/chrome/browser/ui/webui/settings/about_handler.cc
@@ -4,6 +4,20 @@
 
 #include "brave/browser/version_info.h"
 
+namespace {
+
+const char kBraveBuildInstructionsUrl[] = "https://github.com/brave/brave-browser/wiki";
+const char kBraveLicenseUrl[] = "https://mozilla.org/MPL/2.0/";
+const char kBraveReleaseTagPrefix[] = "https://github.com/brave/brave-browser/releases/tag/v";
+
+}
+
+#define BRAVE_LICENSING_INFO base::ASCIIToUTF16(kBraveLicenseUrl), \
+    base::ASCIIToUTF16(kBraveBuildInstructionsUrl), \
+    base::ASCIIToUTF16(kBraveReleaseTagPrefix) + \
+        base::UTF8ToUTF16( \
+            version_info::GetBraveVersionWithoutChromiumMajorVersion()),
+
 #define GetVersionNumber GetBraveVersionNumberForDisplay
 #include "../../../../../../../chrome/browser/ui/webui/settings/about_handler.cc"
 #undef GetVersionNumber
